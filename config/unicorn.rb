b6f66591dc55177b462c03ee96feacf39692430d
Fernando Varani <fervarani@gmail.com>
2015-02-02 12:39:26 -0200

    Change unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,14 @@
+require 'sidekiq'
+
 worker_processes 3
+
+before_fork do |server, worker|
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
 after_fork do |server, worker|
   Sidekiq.configure_client do |config|
     config.redis = { :size => 1 }
@@ -6,4 +16,7 @@ after_fork do |server, worker|
   Sidekiq.configure_server do |config|
     config.redis = { :size => 5 }
   end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
